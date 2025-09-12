6216937d2fded247529f98fbeda624acdf60baaf
Ambar Santos <santosambar738@gmail.com>
2025-09-12 19:26:55 -0400

    Create variables.c

---
+++ variables.c
@@ -0,0 +1,24 @@
+// Practice: Variables 
+// Variables: Boxes that store values
+
+
+#include <stdio.h> 
+
+int main(){
+  
+
+    int age = 20;
+    int year = 2025;
+    float height = 5.0;
+
+
+    printf("Age: %d\n", age);
+    printf("Height: %.2f\n", height);
+    printf("Year: %d\n", year);
+
+
+return 0;
+
+
+
+}
