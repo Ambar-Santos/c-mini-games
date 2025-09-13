5eafbbda2319b55b721c77984a320939b5e9cd98
Ambar Santos <santosambar738@gmail.com>
2025-09-12 23:44:54 -0400

    Update input_practice.c

--- input_practice.c
+++ input_practice.c
@@ -9,24 +9,24 @@
 int main(){
 
 //Variable declaration 
-char name;
+char name[100];
 int favorite_number;
 float height;
 
 
 printf("What's your name?: ");
-  scanf("%c", &name);
+  scanf("%s", name); //arrays already behave like pointers so no &
 
 printf("What is your favorite number of them all?: ");
   scanf("%d", &favorite_number);
 
 printf("What's your height?: ");
-  scanf("%.2f", &height);
+  scanf("%f", &height);
   
 
 //Print everything
 
-printf("Hi, $c, Im very glad to meet you, so you like the number %d, and your height is about %.2f ,nice stuff", name, favorite_number, height);
+printf("Hi, %s, Im very glad to meet you, so you like the number %d, and your height is about %.2f ,nice stuff\n", name, favorite_number, height);
 
 
 
