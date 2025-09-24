ce58133fb9f549c5ac51d81ba4c1e71eb022721b
Ambar Santos <santosambar738@gmail.com>
2025-09-24 16:32:27 -0400

    Update pushups.c

--- practice_challenges/pushups.c
+++ practice_challenges/pushups.c
@@ -5,7 +5,7 @@ int main(){
     int pushups_done = 0;
 
     while (pushups_done < 5){
-        printf("I have done %d pushups! FOOL!\n", pushups );
+        printf("I have done %d pushups! FOOL!\n", pushups_done );
         
         pushups_done = pushups_done + 1;
         
