0b0ab10fb95408a4c35129791e29cee67c674e5c
Ambar Santos <santosambar738@gmail.com>
2025-09-24 13:23:52 -0400

    Create death_scythe.c

---
+++ .vscode/death_scythe.c
@@ -0,0 +1,68 @@
+// Death Scythe Protocol 
+// When a demon weapon has 99 human souls and a witch soul
+// They can become a Death Scythe
+// They now are part of Lord Death personal Arsenal
+
+
+
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+    
+    //Variables 
+    //Soul Data 
+
+    int kishin_eggs = 0;
+    int witch_souls = 0;
+
+   printf("================= Death Scythe Protocol =====================");
+    printf("How many Kishin Egss have you collected: \n");
+      scanf("%d", &kishing_eggs);
+
+     
+    printf("How many Witch Souls have you collected: \n");
+      scanf("%d", &witch_soul);
+
+
+    if (kishin_eggs =>99 && witch_soul == 1){ 
+        // This code runs ONLY if BOTH conditions are TRUE
+        printf("\n*** RESONANCE SUCCESS! ***\n");
+        printf("The soul wavelength is perfect!\n");
+        printf("Your weapon is now a DEATH SCYTHE!\n");
+        printf("Welcome to the ranks of Lord Death's personal arsenal!\n");
+    } else {
+     
+     //If both condicionts are false
+     printf("\n*** RESONANCE FAILED. ***\n");
+     printf("The transformation is incomplete.\n");
+
+     if(kishin_eggs < 99){
+
+        printf("You need %d more Kishing Egg(s).\n",  99 - kishing_eggs);
+
+     } 
+
+     if (has_witch_soul ==0){
+        printf("You mus acquire a Witch's Soul.\n");
+
+     }
+
+     printf("Continue your training!!")
+     
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+printf("===================================================================");
+    return 0;
+}
