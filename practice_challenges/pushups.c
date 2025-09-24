33cc437a198f3db0ef9b267319189005af733272
Ambar Santos <santosambar738@gmail.com>
2025-09-24 16:29:23 -0400

    Create pushups.c

---
+++ practice_challenges/pushups.c
@@ -0,0 +1,17 @@
+//While loop practice 
+#include <stdio.h>
+
+int main(){
+    int pushups_done = 0;
+
+    while (pushups_done < 5){
+        printf("I have done %d pushups! FOOL!\n", pushups );
+        
+        pushups_done = pushups_done + 1;
+        
+    }
+
+ printf("Finally! The tedium is over!\n");
+
+    return 0;
+}
