5d99ccb507d50201e726c4cd1eec674abb89167d
Ambar Santos <santosambar738@gmail.com>
2025-09-24 13:03:09 -0400

    Create soul_resonance.c

---
+++ soul_resonance.c
@@ -0,0 +1,18 @@
+#include <stdio.h> 
+
+int main(){
+ 
+    int kishin_eggs = 99;
+    float sanity_level = 76.5;
+
+    char meister_rank = 'A';
+
+    printf("Kishin Eggs Collected: %d\n", kishin_eggs);
+
+    printf("Current Sanity Level: %.1f\n", sanity_level);
+    
+    printf("Official Meister Rank: %c\n", meister_rank);
+    
+
+    return 0;
+}
