9c42f9bca1fd4bf0a6a520818f5825bc85599f09
Ambar Santos <santosambar738@gmail.com>
2025-09-24 14:23:11 -0400

    Create sanity_checker.c

---
+++ practice_challenges/sanity_checker.c
@@ -0,0 +1,40 @@
+//Intructions from excalibur
+
+/*
+Lord Death is concerned about the madness of wavelengths in Death City. 
+Write a program that does the following:
+Asks the user to input their current sanity level (a number between 0 and 100).
+
+Based on the input, prints a diagnosis:
+
+    If sanity is 80 or higher: "Your soul is sound! A model for DWMA!"
+
+    If sanity is between 50 and 79: "You show signs of strain. Meditate with your weapon."
+
+    If sanity is between 25 and 49: "Madness is approaching! Report to Lord Death immediately!"
+
+    If sanity is below 25: "KISHIN ALERT! CONTAINMENT PROTOCOLS ACTIVATED!"
+
+
+*/
+
+#include <stdio.h>
+
+
+int main(){
+   
+//Variables
+
+int sanity_level;
+
+printf("========================== Sanity Checker =========================\n");
+
+
+
+printf("====================================================================\n");
+
+
+
+
+    return 0;
+}
