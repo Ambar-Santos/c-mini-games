a6ae8732806b1c62d4806e27f0fdbb2fb88c7f32
Ambar Santos <santosambar738@gmail.com>
2025-09-24 17:22:10 -0400

    Changes

---
+++ bro_code/format.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+
+
+int main(){
+
+// format specifier = Sprecial tokens that begin with a % symbol
+//                    followed by a character that specifies the data type,
+//                    and optional modifiers (width, precision, flags).
+//                    They control how data is displayed or interpreted.
+
+int age = 20;
+float price = 19.99;
+double pi = 3.1415926535;
+char currency = '$';
+char name[] = "Ambi";
+
+printf("%d\n", age);
+printf("%f\n", price);
+
+    return 0;
+}
