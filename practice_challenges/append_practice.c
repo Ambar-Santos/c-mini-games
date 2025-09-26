81c02c66592de3cfe3e943afcdbc5f3cf9002cd9
Ambar Santos <santosambar738@gmail.com>
2025-09-26 00:30:10 -0400

    Update append_practice.c

--- practice_challenges/append_practice.c
+++ practice_challenges/append_practice.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 int main(){
    //variables 
@@ -6,6 +7,10 @@ int main(){
    char new_anime[100]; 
    printf("Write the name of the new anime for your list\n");
    fgets(new_anime, sizeof(new_anime), stdin);
+
+   //Remove the newline character if it exists
+
+   new_anime[strcspn(new_anime, "\n")] = 0;
 
    FILE *notebook = fopen("anime_list2.txt", "a");
    fprintf(notebook, "%s", new_anime);
