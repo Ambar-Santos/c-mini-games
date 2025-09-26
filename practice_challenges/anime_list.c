5c686dce45a7dd04ed4b09a002a76f05306a4b87
Ambar Santos <santosambar738@gmail.com>
2025-09-26 00:15:16 -0400

    Create anime_list.c

---
+++ practice_challenges/anime_list.c
@@ -0,0 +1,18 @@
+//File managament practice 
+
+#include <stdio.h>
+
+int main(){
+
+    FILE *notebook = fopen("anime_list.txt", "w");
+
+    fprintf(notebook, "Soul Eater\n");
+    fprintf(notebook, "Fire Force\n");
+    fprintf(notebook, "Gachiakuta\n");
+
+    fclose(notebook);
+
+    printf("Check for anime_list.txt in your foulder!\n");
+
+    return 0;
+}
